06e1e48a42ea01f9065f471ee1587ea473b9222f
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2018-03-20 00:19:16 -0600

    Update BinarySearchTree.java

--- Hoja7/src/hoja7/BinarySearchTree.java
+++ Hoja7/src/hoja7/BinarySearchTree.java
@@ -1,71 +1,75 @@
-package hoja7;
 
-public class BinarySearchTree {
-    // Root of BST
-    Node root;
- 
-    // Constructor
-    public BinarySearchTree() { 
-        this.root = null; 
+public class BinaryTree<K extends Comparable<K>, V> {
+    public Node<K,V> root;
+
+    public BinaryTree(){
+        this.root = null;
     }
+
  
-    // This method mainly calls insertRec()
-    public void insert(Association key) {
-       this.root = insertRec(this.root, key);
-    }
-     
-    /* A recursive function to insert a new key in BST */
-    public Node insertRec(Node root, Association key) {
- 
-        /* If the tree is empty, return a new node */
-        if (root == null) {
-            root = new Node(key);
-            return root;
+    public V find(K key){
+        Node<K,V> current = root;
+        while(current != null){
+            int comparation = current.data.compareTo(key);  // Comparar la palabra con la llave de la asociacion en el nodo
+            if(comparation == 0){
+                return current.data.getValue();
+            }else if(comparation > 0){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
         }
- 
-        /* Otherwise, recur down the tree */
-        if (((String)key.getKey()).compareTo((String)root.key.getKey())==-1)
-            root.left = insertRec(root.left, key);
-        else if (((String)key.getKey()).compareTo((String)root.key.getKey())==1)
-            root.right = insertRec(root.right, key);
- 
-        /* return the (unchanged) node pointer */
-        return root;
+        return null;
     }
- 
-    // This method mainly calls InorderRec()
-    public void inorder()  {
-       inorderRec(root);
-    }
-    
-    // A utility function to do inorder traversal of BST
-    public void inorderRec(Node root) {
-        if (root != null) {
-            inorderRec(root.left);
-            System.out.println(root.key);
-            inorderRec(root.right);
+
+
+    public void insert(K key, V value){
+        Node<K,V> newNode = new Node<K,V>(key, value);
+        if(root==null){
+            root = newNode;
+            return;
+        }
+
+        Node<K,V> current = root;
+        Node<K,V> parent = null;
+        while(true){
+            parent = current;
+            int comparation = current.data.compareTo(key);
+            if(comparation > 0){
+                current = current.left;
+                if(current==null){
+                    parent.left = newNode;
+                    return;
+                }
+            }else{
+                current = current.right;
+                if(current==null){
+                    parent.right = newNode;
+                    return;
+                }
+            }
         }
     }
-    
-    public Node buscar(String key){
-        return search(this.root,key);
-    }
-    
-    
-    public Node search(Node root, String key)
-    {
-        // Base Cases: root is null or key is present at root
-        if (root==null || root.key.getKey().equals(key))
-            return root;
 
-        
-        // val is greater than root's key
-        if (((String)root.key.getKey()).compareTo((String)key)==1)
-            return search(root.left, key);
-
-        // val is less than root's key
-        return search(root.right, key);
+ 
+    public void display(Node<K,V> root){
+        if(root!=null){
+            display(root.left);
+            System.out.print(" " + root.data.toString());
+            display(root.right);
+        }
     }
-    
-    
 }
+
+
+class Node<K extends Comparable<K>,V>{
+    Association<K, V> data;
+    Node<K,V> left;
+    Node<K,V> right;
+    public Node(K key, V value){
+        this.data = new Association<K, V>(key, value);
+        left = null;
+        right = null;
+    }
+} 
+
